a6a99c984c63f0374b966b1bb9443bb270ce945f
vikylorentseaputra <vikylorentseaputra@gmail.com>
2024-02-27 21:43:19 +0700

    feat : update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import HomeComponent from './component/HomeComponent'
 import ImportComponent from './component/ImportComponent';
 
 function App() {
 
   return (
     <main>
-      {/* <HomeComponent/> */}
       <ImportComponent/>
     </main>
   
